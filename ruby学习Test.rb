0b4003b0c059c55be402f67f8b029be144bf329b
WangBoguo <wangboguo@gmail.com>
2016-08-29 21:38:01 +0900

    学习Ruby

---
+++ ruby学习Test.rb
@@ -0,0 +1,20 @@
+# 定义一个名为yeller的方法,它的参数是一个由字符组成的数组,返回值是一个字符串,由数组中字 符的大写形式组成。确认yeller([’o’, ’l’, ’d’])的返回值是OLD。提示:要用到map、upcase和 join 方法。
+def yello(str = [])
+	str.map{|char| char.upcase}
+end
+
+puts yello(["a","b","c"])
+
+def random_subdomain(str = [])
+	str.to_a.shuffle[0..7].join
+end
+puts random_subdomain('a'..'z')
+
+def string_shuffle(s)
+	s.split('').shuffle.join
+end
+puts string_shuffle('foobar')
+a="adasfas"
+puts a.class
+puts a.split('').class
+puts a.split('').shuffle.join.class
